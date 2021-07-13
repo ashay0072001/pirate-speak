2ec0cf47bd6f3b4bce49e74bffcdc6ed52ae6f24
ashay0072001 <84988287+ashay0072001@users.noreply.github.com>
2021-07-13 19:00:19 +0530

    done

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var txtbox = document.querySelector("#textBox")
-var button = document.querySelector("#buttonClick");
-var outputt = document.querySelector("#outPut")
+var btnResponse = document.querySelector("#buttonClick")
+var textReader = document.querySelector("#textBox")
+console.log("text",textReader.value)
+var outputReader = document.querySelector("#outPut")
 
 
+var serverURL = "https://api.funtranslations.com/translate/pirate.json"
+function getTranslationURL(text) {
+    console.log(typeof(text))
+    
+    return `${serverURL}?text=${text}`
+}
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something went wrong try after sometime")
+}
+
+function clickEventHandler() {
+    
+    
+    var inputText = textReader.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputReader.innerText = translatedText;
+        })
+           
+        
+        .catch(errorHandler)
+};
+
+buttonClick.addEventListener("click", clickEventHandler)
